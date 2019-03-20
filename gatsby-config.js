7abd04ea5576c1b4363858d495e2b5c7f6bd230d
Jaakko Malkki <jaakko.malkki234@gmail.com>
2019-03-21 00:30:58 +0200

    Add + configure 'gatsby-plugin-manifest'

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,19 @@ module.exports = {
       resolve: `gatsby-plugin-typography`,
       options: { pathToConfigModule: `src/utils/typography.js` }
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Jaakko Malkki`,
+        short_name: `Jaakko Malkki`,
+        start_url: `/`,
+        background_color: `#000000`,
+        theme_color: `#845c21`,
+        display: `browser`,
+        icon: `src/images/icon.png`,
+        include_favicon: true
+      }
+    },
     `gatsby-plugin-offline`
   ]
 };
