fe256dc964f44a050c190c55d4f717483519a387
Jaakko Malkki <jaakko.malkki234@gmail.com>
2019-03-21 19:46:57 +0200

    Add SEO component to pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Layout from "../components/layout";
 import GithubEvents from "../components/GithubEvents";
+import SEO from "../components/SEO";
 
 export default () => (
-  <Layout>
-    <GithubEvents user="mjaakko" />
-  </Layout>
+  <>
+    <SEO pagePath="/" />
+    <Layout>
+      <GithubEvents user="mjaakko" />
+    </Layout>
+  </>
 );
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 import Layout from "../components/layout";
+import SEO from "../components/SEO";
 
 export default () => (
-  <Layout>
-    <p>Nothing yet :(</p>
-  </Layout>
+  <>
+    <SEO pagePath="/projects" pageTitle="Projects" />
+    <Layout>
+      <p>Nothing yet :(</p>
+    </Layout>
+  </>
 );
