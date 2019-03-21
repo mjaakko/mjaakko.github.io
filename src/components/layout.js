4e1c4c52036dd7f5c8fc861d7bbb8a647d5af280
Jaakko Malkki <jaakko.malkki234@gmail.com>
2019-03-21 18:55:42 +0200

    Add Header to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,12 @@
 import React from "react";
 
+import Header from "./Header";
+
 export default ({ children }) => (
-  <div style={{ margin: `0 auto`, maxWidth: 1000, padding: `0 1rem` }}>
-    {children}
-  </div>
+  <>
+    <Header />
+    <div style={{ margin: `0 auto`, maxWidth: 1000, padding: `0 2rem` }}>
+      {children}
+    </div>
+  </>
 );
