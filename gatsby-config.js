a67c3b2e257e533bc2f03282a272026e3b6bd0c3
Jaakko Malkki <jaakko.malkki234@gmail.com>
2019-04-04 19:17:01 +0300

    Add + configure gatsby-plugin-styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     ]
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-typography`,
       options: { pathToConfigModule: `src/utils/typography.js` }
