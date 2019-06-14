712507fef697d7f78c7ee3c4ee80b4c06d689cd4
Jaakko Malkki <jaakko.malkki234@gmail.com>
2019-06-14 19:43:38 +0300

    Add className prop to MenuButton

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -38,6 +38,7 @@ export default class extends React.PureComponent {
   render() {
     return (
       <MenuButton
+        className={this.props.className}
         size={this.props.size}
         onClick={() =>
           this.setState(state => {
