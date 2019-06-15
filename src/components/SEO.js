64ed9edfbf41e6e737331f545e573795a602d71a
Jaakko Malkki <jaakko.malkki234@gmail.com>
2019-06-15 13:45:45 +0300

    Refactor SEO component to use useSiteMetadata hook

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,50 +1,36 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import { StaticQuery, graphql } from "gatsby";
 
-export default ({ pageTitle, pageDescription, pagePath }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        site {
-          siteMetadata {
-            siteUrl
-            title
-          }
-        }
-      }
-    `}
-    render={({ site: { siteMetadata: metadata } }) => {
-      const title = pageTitle
-        ? `${pageTitle} | ${metadata.title}`
-        : metadata.title;
+import useSiteMetadata from "../hooks/useSiteMetadata";
 
-      const canonicalUrl = metadata.siteUrl + pagePath;
+export default ({ pageTitle, pageDescription, pagePath }) => {
+  const metadata = useSiteMetadata();
 
-      const description =
-        pageDescription || "Personal website of Jaakko Malkki";
+  const title = pageTitle ? `${pageTitle} | ${metadata.title}` : metadata.title;
 
-      return (
-        <Helmet>
-          <html lang="en" prefix="og: http://ogp.me/ns#" />
-          <meta property="og:url" content={canonicalUrl} />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content={title} />
-          <meta property="og:description" content={description} />
-          {/* <meta property="og:image" content={shareImageUrl} /> */}
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:title" content={title} />
-          <meta name="twitter:description" content={description} />
-          {/* <meta name="twitter:image" content={shareImageUrl} /> */}
-          <title>{title}</title>
-          <meta name="description" content={description} />
-          <link rel="canonical" href={canonicalUrl} />
-          <meta
-            name="google-site-verification"
-            content="7plJI9PIjUYQfvMz5-tplgZZ5kEGyE7zxOK5kps1CXM"
-          />
-        </Helmet>
-      );
-    }}
-  />
-);
+  const canonicalUrl = metadata.siteUrl + pagePath;
+
+  const description = pageDescription || "Personal website of Jaakko Malkki";
+
+  return (
+    <Helmet>
+      <html lang="en" prefix="og: http://ogp.me/ns#" />
+      <meta property="og:url" content={canonicalUrl} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      {/* <meta property="og:image" content={shareImageUrl} /> */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      {/* <meta name="twitter:image" content={shareImageUrl} /> */}
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <link rel="canonical" href={canonicalUrl} />
+      <meta
+        name="google-site-verification"
+        content="7plJI9PIjUYQfvMz5-tplgZZ5kEGyE7zxOK5kps1CXM"
+      />
+    </Helmet>
+  );
+};
