1bfd4deb07ac8ded37b37b62162d65034b78d2b3
Jaakko Malkki <jaakko.malkki234@gmail.com>
2019-07-23 19:16:23 +0300

    Update Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,39 @@
 import React from "react";
 
+import useContacts from "../hooks/useContacts";
+
 import Layout from "../components/layout";
 import SEO from "../components/SEO";
 import BreadcrumbList from "../components/BreadcrumbList";
 
-export default () => (
-  <>
-    <SEO pagePath="/contact" pageTitle="Contact" />
-    <BreadcrumbList breadcrumbs={[{ name: "Contact", path: "/contact/" }]} />
-    <Layout>
-      <p>Nothing yet :(</p>
-    </Layout>
-  </>
-);
+export default () => {
+  const contacts = useContacts();
+
+  return (
+    <>
+      <SEO
+        pagePath="/contact"
+        pageTitle="Contact"
+        pageDescription="Contact details"
+      />
+      <BreadcrumbList breadcrumbs={[{ name: "Contact", path: "/contact/" }]} />
+      <Layout>
+        <h1>Contact</h1>
+        <ul>
+          {contacts.map(contact => (
+            <li key={contact.type}>
+              <img
+                src={contact.icon.publicURL}
+                alt=""
+                height={20}
+                width={20}
+                style={{ marginRight: 4, margin: 0, verticalAlign: "middle" }}
+              />
+              <a href={contact.link}>{contact.username}</a>
+            </li>
+          ))}
+        </ul>
+      </Layout>
+    </>
+  );
+};
