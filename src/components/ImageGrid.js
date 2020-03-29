46dc0ba813d4e1262c362e792d68fa029172b937
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2020-03-29 19:18:36 +0300

    Create component for showing grid of images

---
+++ src/components/ImageGrid.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Img from "gatsby-image";
+import ImageModal from "../components/ImageModal";
+
+const ImageGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 290px);
+  grid-template-rows: repeat(auto-fill, 170px);
+  grid-row-gap: 1rem;
+  grid-column-gap: 1rem;
+`;
+
+const ImageContainer = styled.div`
+  width: 290px;
+  height: 170px;
+`;
+
+const Image = ({ image }) => {
+  const [showFull, setShowFull] = useState(false);
+
+  return (
+    <>
+      <ImageModal
+        image={image.image.large.fluid}
+        description={image.description}
+        visible={showFull}
+        onClickOutside={() => setShowFull(false)}
+      />
+      <ImageContainer onClick={() => setShowFull(true)}>
+        <Img
+          fixed={image.image.thumbnail.fixed}
+          alt=""
+          style={{ width: 290, height: 170 }}
+          imgStyle={{ margin: 0 }}
+        />
+      </ImageContainer>
+    </>
+  );
+};
+
+export default ({ images }) => (
+  <ImageGrid>
+    {images.map(image => {
+      return <Image image={image} />;
+    })}
+  </ImageGrid>
+);
